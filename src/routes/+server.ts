a45b77012786ac3e9f3a89f2a91590299a4c878d
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-12-06 23:00:39 +0500

    server routes for get and create

---
+++ src/routes/+server.ts
@@ -0,0 +1,25 @@
+import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { createPaste } from '$lib/firebase/app';
+
+export const POST: RequestHandler = async ({ request }) => {
+    const formData = await request.formData();
+    let file;
+    let count = 0;
+
+    for (const entry of formData.entries()) {
+        if (entry[1] instanceof File) {
+            file = entry[1];
+            count++;
+        }
+    }
+
+    if (file === undefined || count !== 1) {
+        throw error(400, 'one and only one file should be passed.');
+    }
+
+    const fileContent = await file.text();
+    const paste = await createPaste(fileContent)
+
+    return json({ id: paste });
+};
---
+++ src/routes/r/[id]/+server.ts
@@ -0,0 +1,20 @@
+import { error } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import { getPaste } from '$lib/firebase/app';
+
+export const GET: RequestHandler = async ({ params }) => {
+    const id = params.id;
+    if (id === undefined) {
+        throw error(400, 'id is required.');
+    }
+    const paste = await getPaste(id)
+    if (paste === null) {
+        throw error(404, 'Not found');
+    }
+
+    return new Response(paste.content,  {
+        headers: {
+            'Content-Type': 'text/plain'
+        }
+    })
+};
