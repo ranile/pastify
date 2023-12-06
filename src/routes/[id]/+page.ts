b0f8e96a6e5831cfffc22a78f675e9ed7b879d7a
Muhammad Hamza <muhammadhamza1311@gmail.com>
2023-12-06 23:08:13 +0500

    language hints for syntax highlighting

--- src/routes/[id]/+page.ts
+++ src/routes/[id]/+page.ts
@@ -4,13 +4,59 @@ import { getPaste } from '$lib/firebase/app';
 import detectLang from 'lang-detector';
 
 export const load: PageLoad = async ({ params }) => {
-  const paste = await getPaste(params.id);
-  if (paste === null) {
-    throw error(404, 'Not found');
-  }
-  const lang = detectLang(paste.content);
-  return {
-    paste,
-    language: lang
-  };
+    let languageHint;
+    let id;
+    if (params.id.includes('.')) {
+        const [pasteId, lang] = params.id.split('.');
+        id = pasteId;
+        languageHint = lang;
+    } else {
+        id = params.id
+    }
+
+    const paste = await getPaste(id);
+    if (paste === null) {
+        throw error(404, 'Not found');
+    }
+
+
+    const lang = languageHint ? mapLangHintToLanguage(languageHint) : detectLang(paste.content);
+    return {
+        paste,
+        language: lang
+    };
 };
+
+function mapLangHintToLanguage(hint: string) {
+    // use extensions: js, ts, cpp, c, json, py, kt, rs, html, css, rb, go, php
+    switch (hint) {
+        case 'js':
+            return 'javascript';
+        case 'ts':
+            return 'typescript';
+        case 'cpp':
+            return 'c++';
+        case 'c':
+            return 'c';
+        case 'json':
+            return 'json';
+        case 'py':
+            return 'python';
+        case 'kt':
+            return 'kotlin';
+        case 'rs':
+            return 'rust';
+        case 'html':
+            return 'html';
+        case 'css':
+            return 'css';
+        case 'rb':
+            return 'ruby';
+        case 'go':
+            return 'go';
+        case 'php':
+            return 'php';
+        default:
+            return 'plaintext';
+    }
+}
